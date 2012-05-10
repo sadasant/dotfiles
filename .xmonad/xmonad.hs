3b20c9e2b34570c7c2fba516171243a1d7e667e1
sadasant <sadasant@gmail.com>
2012-05-09 23:31:49 -0430

    added xmonad config

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,31 @@
+-- Imports
+import XMonad
+import XMonad.Hooks.DynamicLog
+
+-- The Main Function
+main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
+
+-- Launch bar
+-- myBar = "xmobar"
+myBar = "xmobar & sleep 5; conky" -- lol this works :D
+                                  -- remember to empty the template in .xmobarrc
+
+-- Custom PP
+myPP = xmobarPP { ppCurrent = xmobarColor "#FFFFFF" "" }
+
+-- Key binding to toggle the gap for the bar
+toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
+
+-- Hooks
+myManageHook = composeAll
+  [ className =? "Gimp" --> doFloat
+  ]
+
+-- Main configuration
+myConfig = defaultConfig
+  { terminal           = "xterm"
+  , modMask            = mod4Mask
+  , normalBorderColor  = "#111111"
+  , focusedBorderColor = "#333333"
+  , manageHook         = myManageHook
+  }
