fa2b6194c1dd62f960f1fc9ef422d1f8ccc5042a
Daniel Rodríguez <sadasant@gmail.com>
2012-09-18 00:36:19 -0430

    Well, it was bad. #fail

--- .vim/my_plugins.sh
+++ .vim/my_plugins.sh
@@ -8,12 +8,18 @@ repos=(
   "ciaranm/detectindent"
   )
 
+if [ ! -d ./bundle ]
+then
+  mkdir bundle
+fi
+
+cd bundle
 for repo in "${repos[@]}"
 do
   repo_name=`echo ${repo} | cut -d '/' -f 2`
   if [ ! -d ./bundle/$repo_name ]
   then
-    git clone git@github.com/"${repo}"
+    git clone git@github.com:"${repo}"
   else
     echo Exists: "${repo}"
   fi
