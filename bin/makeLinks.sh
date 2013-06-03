fcb7bd782b6761e83f80ddead0883a051fd49d5a
Daniel Rodriguez <sadasant@gmail.com>
2013-06-03 09:09:01 -0430

    Making makeLinks user friendly

--- bin/makeLinks.sh
+++ bin/makeLinks.sh
@@ -1,10 +1,10 @@
 # Make the links
 
-ln -sf /home/sadasant/code/github/sadasant/dotfiles/.bashrc ~/.bashrc
-ln -sf /home/sadasant/code/github/sadasant/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf /home/sadasant/code/github/sadasant/dotfiles/.gitconfig ~/.gitconfig
-ln -sf /home/sadasant/code/github/sadasant/dotfiles/.vimrc ~/.vimrc
+ln -sf /home/$USER/code/github/sadasant/dotfiles/.bashrc ~/.bashrc
+ln -sf /home/$USER/code/github/sadasant/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf /home/$USER/code/github/sadasant/dotfiles/.gitconfig ~/.gitconfig
+ln -sf /home/$USER/code/github/sadasant/dotfiles/.vimrc ~/.vimrc
 if [ ! -d ~/.vim ]
 then
-  ln -sf /home/sadasant/code/github/sadasant/dotfiles/.vim ~/.vim
+  ln -sf /home/$USER/code/github/sadasant/dotfiles/.vim ~/.vim
 fi
