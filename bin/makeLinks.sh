d79b4636096fb5398901b6b0b113d976869b3329
Daniel Rodriguez <sadasant@gmail.com>
2013-06-17 08:43:10 -0430

    uzbl configs

--- bin/makeLinks.sh
+++ bin/makeLinks.sh
@@ -3,12 +3,20 @@
 ln -sf /home/$USER/code/github/sadasant/dotfiles/.bashrc ~/.bashrc
 ln -sf /home/$USER/code/github/sadasant/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf /home/$USER/code/github/sadasant/dotfiles/.gitconfig ~/.gitconfig
+
+# vim dirs
 ln -sf /home/$USER/code/github/sadasant/dotfiles/.vimrc ~/.vimrc
 if [ ! -d ~/.vim ]; then
   ln -sf /home/$USER/code/github/sadasant/dotfiles/.vim ~/.vim
 fi
 
-# Go Dirs
+# uzbl dirs
+if [ ! -d ~/.config ]; then
+  mkdir ~/.config
+fi
+ln -sf /home/$USER/code/github/sadasant/dotfiles/.config/uzbl ~/.config/uzbl
+
+# go dirs
 if [ ! -d ~/code/go ]; then
   mkdir ~/code/go
 fi
