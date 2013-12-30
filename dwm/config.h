71a9efe3781aae84e2906c1571a68dbf7917eef0
Daniel Rodrí­guez <sadasant@gmail.com>
2013-12-30 03:18:45 -0430

    xbindkeysrc, xinit and dwm changes

--- dwm/config.h
+++ dwm/config.h
@@ -50,7 +50,10 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+// static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
+// static const char *termcmd[]  = { "st", NULL };
 static const char *termcmd[]  = { "uxterm", NULL };
 
 static Key keys[] = {
