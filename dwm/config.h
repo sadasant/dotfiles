7261890a34df487a2d2e46fe80132cf5847bf61d
Daniel Rodrí­guez <sadasant@gmail.com>
2014-06-18 09:08:01 -0430

    dwm font

--- dwm/config.h
+++ dwm/config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const char font[]            = "-*-terminus-medium-r-*-*-16-*-*-*-*-*-*-*";
+static const char font[]            = "-*-terminus-medium-r-*-*-12-*-*-*-*-*-*-*";
 static const char normbordercolor[] = "#121212"; // 233
 static const char normbgcolor[]     = "#121212"; // 233
 static const char normfgcolor[]     = "#585858"; // 240
