746c5f673f83c9211de5030acb07e2e768fcc987
Daniel Rodrí­guez <sadasant@gmail.com>
2014-07-09 10:47:23 -0430

    Sending chrome stuff into desktop 9 to avoid random appearings of hangout windows

--- dwm/config.h
+++ dwm/config.h
@@ -23,7 +23,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
- 	{ "Firefox",  NULL,       NULL,       0,            False,       -1 },
+ 	{ "Chromium", NULL,       NULL,       1 << 8,       False,       -1 },
 // 	{ "Firefox",  NULL,       NULL,       1 << 8,       False,       -1 },
 	{ "Xmessage", NULL,       NULL,       ~0,           True,        -1 },
 };
